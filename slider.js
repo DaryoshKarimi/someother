a360b629696bd2a7e0f3ecfbbe19952af6f69271
DaryoshKarimi <50103419+DaryoshKarimi@users.noreply.github.com>
2021-12-12 12:59:36 +1100

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,53 @@
+const list_of_images = [
+  'images/4.png',
+  'images/3.jpg',
+  'images/1.jpg',
+  'images/2.png',
+]
+
+
+
+const image_cont = document.createElement('div');
+document.body.appendChild(image_cont);
+image_cont.className = 'image_cont';
+
+
+
+
+// Turns urls into image els on page so that they all load when the page loads.
+
+const list_of_els = [];
+
+for (let i=0; i<list_of_images.length; i++) {
+  const el = document.createElement('div');
+  el.className = 'image';
+
+  image_cont.appendChild(el);
+  el.style.backgroundImage = "url(" + list_of_images[i] + ")";
+  list_of_els[i] = el;
+}
+
+
+
+
+
+
+
+// Animates images
+
+let i = 0;
+let previous_image = null;
+
+window.setInterval(
+  function () {
+    list_of_els[i].style.display = 'block';
+    if (previous_image != null) {
+      previous_image.style.display = 'none';
+    }
+
+    previous_image = list_of_els[i];
+    i = (i + 1) % list_of_els.length;
+  },
+  500
+);
+
