dbdadb8e02dd7ad3929993540fec3b4a43f3f26b
Daryosh <daryoosh.ka@gmail.com>
2021-12-12 04:18:58 -0500

    clock

--- slider.js
+++ slider.js
@@ -58,3 +58,12 @@ window.setInterval(
   220
 );
 
+
+var myVar = setInterval(function() {
+  myTimer();
+}, 1000);
+
+function myTimer() {
+  var d = new Date();
+  document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+}
